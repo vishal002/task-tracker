72f11af2a6472cd0551cfe119a7525cdca2bbf28
Vishal Kumar <vishal.kumar@qio.io>
2024-02-23 23:31:54 +0530

    ui added

---
+++ src/components/AddTask.js
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+const AddTask = ({ onAdd }) => {
+  const [text, setText] = useState("");
+  const [day, setDay] = useState("");
+  const [reminder, setReminder] = useState(false);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (!text) {
+      alert("please add a task");
+      return;
+    }
+
+    onAdd({ text, day, reminder });
+
+    setText("");
+    setDay("");
+    setReminder(false);
+  };
+
+  return (
+    <form className="add-form" onSubmit={onSubmit}>
+      <div className="form-control">
+        <label>Task</label>
+        <input
+          type="text"
+          value={text}
+          placeholder="Add Task"
+          onChange={(e) => setText(e.target.value)}
+        />
+      </div>
+      <div className="form-control">
+        <label>Day & Time</label>
+        <input
+          type="text"
+          value={day}
+          placeholder="Add Day & Time"
+          onChange={(e) => setDay(e.target.value)}
+        />
+      </div>
+      <div className="form-control form-control-check">
+        <label>Set Reminder</label>
+        <input
+          type="checkbox"
+          checked={reminder}
+          value={reminder}
+          onChange={(e) => setReminder(e.currentTarget.checked)}
+        />
+      </div>
+
+      <input type="submit" value="Save Task" className="btn btn-block" />
+    </form>
+  );
+};
+
+export default AddTask;
---
+++ src/components/Button.js
@@ -0,0 +1,25 @@
+import PropsTypes from "prop-types";
+
+const Button = ({ color, text, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: color }}
+      className="btn"
+    >
+      {text}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  color: "steelblue",
+};
+
+Button.propTypes = {
+  text: PropsTypes.string,
+  color: PropsTypes.string,
+  onClick: PropsTypes.func
+};
+
+export default Button;
